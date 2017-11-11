971b23bb957d4bfc5424292b2041f4d898950e45
Isil Donmez <isil.donmez@gmail.com>
2017-11-11 01:14:47 +0000

    Change class Node

--- knights_tree.rb
+++ knights_tree.rb
@@ -1,18 +1,32 @@
 class Node
-  attr_accessor :array, :parent_node, :coordinate_options
+  attr_accessor :coordinate, :parent_node, :coordinate_options
 
-    def initialize(array, parent_node = nil, coordinate_options = [])
-      @array = array
+    def initialize(coordinate, parent_node = nil, coordinate_options = [])
+      @coordinate = coordinate
       @parent_node = parent_node
       @coordinate_options = coordinate_options
     end
 
-    def self.build_tree(starting_point)
+    # TODO
+    def self.build_tree(starting_point, ending point)
       head = Node.new(starting_point)
-
+      current_node = head
 
       to_add = [ [-1, -2], [1, -2], [1, 2], [-1, 2]
                 [2, -1], [2, 1], [-2, 1], [-2, -1] ]
-      head.
+
+      current_node.coordinate_options = to_add.map do |coord|
+        coord[0] = starting_point[0] + coord[0]
+        coord[1] = starting_point[1] + coord[1]
+      end.compact
+
+      return head if current_node.coordinate_options.include? ending_point
+
+      current_node.coordinate_options.each do |coord|
+        Node.new(coord, current_node)
+      end
+
+
+
     end
 
