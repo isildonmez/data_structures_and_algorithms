2d459be64d6699b307b301a6f62dbce68dec4166
Isil Donmez <isil.donmez@gmail.com>
2017-11-11 22:48:24 +0000

    Finish build_path method in knights_tree.rb

--- knights_travails.rb
+++ knights_travails.rb
@@ -1,6 +1,6 @@
-load 'knights_tree.rb'
+require_relative 'knights_tree'
 
 def knight_moves(starting_point, ending_point)
-  Node.build_tree(starting_point)
+  Node.build_path(starting_point, ending_point)
 
 end
--- knights_tree.rb
+++ knights_tree.rb
@@ -1,32 +1,39 @@
 class Node
   attr_accessor :coordinate, :parent_node, :coordinate_options
 
-    def initialize(coordinate, parent_node = nil, coordinate_options = [])
-      @coordinate = coordinate
-      @parent_node = parent_node
-      @coordinate_options = coordinate_options
-    end
+  def initialize(coordinate, parent_node = nil, coordinate_options = [])
+    @coordinate = coordinate
+    @parent_node = parent_node
+    @coordinate_options = coordinate_options
+  end
 
-    # TODO
-    def self.build_tree(starting_point, ending point)
-      head = Node.new(starting_point)
-      current_node = head
+  def self.build_path(starting_point, ending_point)
+    head = Node.new(starting_point)
+    queue = []
+    queue << head
+    to_add = [ [-1, -2], [1, -2], [1, 2], [-1, 2],
+              [2, -1], [2, 1], [-2, 1], [-2, -1] ]
 
-      to_add = [ [-1, -2], [1, -2], [1, 2], [-1, 2]
-                [2, -1], [2, 1], [-2, 1], [-2, -1] ]
+    until queue.empty?
+      current_node = queue.shift
 
       current_node.coordinate_options = to_add.map do |coord|
-        coord[0] = starting_point[0] + coord[0]
-        coord[1] = starting_point[1] + coord[1]
+        new_coord = []
+        new_coord[0] = current_node.coordinate[0] + coord[0]
+        new_coord[1] = current_node.coordinate[1] + coord[1]
+        new_coord = nil unless new_coord.any? {|num| num.between?(0,7)}
+        new_coord
       end.compact
 
-      return head if current_node.coordinate_options.include? ending_point
-
       current_node.coordinate_options.each do |coord|
-        Node.new(coord, current_node)
+        queue << Node.new(coord, current_node)
       end
-
-
-
+      return current_node if current_node.coordinate_options.include? ending_point
     end
+  end
+
+end
+
+# p Node.build_path([3,3], [2,5])
+# p Node.build_path([3,3], [3,7])
 
